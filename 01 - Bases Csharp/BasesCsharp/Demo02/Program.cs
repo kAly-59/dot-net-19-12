998e14df5092b3edbb478b705fc7654335617774
Guillaume MAIRESSE <guillaume@utopios.solutions>
2023-12-19 16:37:23 +0100

    Bases cs - sujet exo1

--- 01 - Bases Csharp/BasesCsharp/Demo02/Program.cs
+++ 01 - Bases Csharp/BasesCsharp/Demo02/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Je m'appelle \"Guillaume\"");
+Console.WriteLine('\'');
+Console.WriteLine("c:\\repertoire\\fichier.cs");
+Console.WriteLine(@"c:\repertoire\fichier.cs");
