45f9a8bee2425982e9164696804f811ca604f845
Guillaume MAIRESSE <guillaume@utopios.solutions>
2023-12-20 11:52:13 +0100

    Bases cs - correction 2 3 4 5 + exo 6 7 8 9

---
+++ 01 - Bases Csharp/BasesCsharp/Exercice02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Veuillez saisir votre prénom : ");
+//string firstname = Console.ReadLine();
+//Console.Write("Bonjour ");
+//Console.WriteLine(firstname);
+
+
+Console.WriteLine("Bonjour " + Console.ReadLine());
