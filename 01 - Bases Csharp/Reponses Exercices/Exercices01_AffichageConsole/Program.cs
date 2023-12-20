1523aa4646dd02c6d618977287a8bb7bdb58be4c
kAly <kev.callet@gmail.com>
2023-12-20 10:00:58 +0100

    doc; add correction

--- 01 - Bases Csharp/Reponses Exercices/Exercices01_AffichageConsole/Program.cs
+++ 01 - Bases Csharp/Reponses Exercices/Exercices01_AffichageConsole/Program.cs
@@ -1,8 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*** Ma Liste de todo ***");
+﻿Console.WriteLine("*** Ma Liste de todo ***");
 Console.WriteLine("Aujourd'hui je dois faire:");
 Console.WriteLine("\t -Apprendre le C#");
 Console.WriteLine("\t -Apprendre à utiliser Visual Studio");
 Console.WriteLine("\t -Comprendre l'affichage \"Console\"");
-Console.WriteLine("\t -Créer mon répertoire \"c:\\MesExercices\\\" pour les rangers");
-Console.WriteLine("\t -Apprécier les fonctionnalités du C#");
+Console.WriteLine("\t -Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger");
