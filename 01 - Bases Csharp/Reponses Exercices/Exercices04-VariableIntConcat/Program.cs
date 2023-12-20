1bafccc78053269bc6456dae95ddf9b1abd341e2
kAly <kev.callet@gmail.com>
2023-12-20 10:34:04 +0100

    doc: update exo

---
+++ 01 - Bases Csharp/Reponses Exercices/Exercices04-VariableIntConcat/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Veuillez saisir votre nom :");
+string nameInput = Console.ReadLine();
+
+Console.WriteLine("Veuillez saisir votre prénom :");
+string prenomInput = Console.ReadLine();
+
+Console.WriteLine("Veuillez saisir votre âge :");
+int ageInput = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Bonjour " + nameInput + " " + prenomInput + ", vous avez " + ageInput + " ans.");
---
+++ 01 - Bases Csharp/Reponses Exercices/Exercices05-VariableNumerique/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Veuillez saisir un nombre :");
+int nb1Input = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Veuillez saisir un nombre:");
+int nb2Input = int.Parse(Console.ReadLine());
+
+int somme = nb1Input + nb2Input;
+
+Console.WriteLine("Resultat : " + somme);
