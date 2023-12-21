86b1173030e56ad4330948075d261f4b5de5f399
Guillaume MAIRESSE <guillaume@utopios.solutions>
2023-12-21 11:08:11 +0100

    Bases cs - corrections 10 11 12

---
+++ 01 - Bases Csharp/BasesCsharp/Exercice11/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("*** Le nombre est-il divisible par...? ***\n");
+Console.Write("Entrez un chiffre/nombre entier : ");
+//int nbEntier = Convert.ToInt32(Console.ReadLine());
+int nbEntier;
+bool saisieValide = int.TryParse(Console.ReadLine(), out nbEntier);
+
+Console.Write("Entrez un chiffre/nombre diviseur : ");
+//int nbDiviseur = Convert.ToInt32(Console.ReadLine());
+int nbDiviseur;
+saisieValide = saisieValide & int.TryParse(Console.ReadLine(), out nbDiviseur);
+
+
+if (!saisieValide)
+{
+    Console.WriteLine("Saisie invalide");
+    return;
+    // on quitte la méthode Main de la classe Program
+    // permet d'arréter notre application
+    //Environment.Exit(0); // permet aussi d'arréter notre application
+}
+
+
+Console.WriteLine();
+
+// TERNAIRE
+// var variable = <Condition> ? <Valeur si Vrai> : <Valeur si Faux>;
+string chiffreOuNombre = (-10 < nbEntier && nbEntier < 10) ? "chiffre" : "nombre";
+// équivalent:
+//string chiffreOuNombre = "nombre";
+//if (-10 < nbEntier && nbEntier < 10)
+//    chiffreOuNombre = "chiffre";
+
+if (nbEntier % nbDiviseur == 0)
+    Console.WriteLine($"Le {chiffreOuNombre} est divisible par {nbDiviseur}");
+else
+    Console.WriteLine($"Le {chiffreOuNombre} n'est pas divisible par {nbDiviseur}");
+
+string divisibleOuNon = (nbEntier % nbDiviseur == 0) ? "est" : "n'est pas";
+
+Console.WriteLine($"Le {chiffreOuNombre} {divisibleOuNon} divisible par {nbDiviseur}");
---
+++ 01 - Bases Csharp/BasesCsharp/Exercice12/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("--- Dans quelle catégorie mon enfant est il ...? ---");
+Console.Write("Entrez l'age de l'enfant : ");
+//int age = Convert.ToInt32();
+
+if (!int.TryParse(Console.ReadLine(), out int age) || age < 0 || age > 125 )
+{
+    Console.WriteLine("Saisie invalide");
+    return;
+}
+
+if (age < 3)
+    Console.WriteLine("Vous êtes trop jeune");
+else if (age <= 6)
+    Console.WriteLine("Baby");
+else if (age <= 8)
+    Console.WriteLine("Poussin");
+else if (age <= 10)
+    Console.WriteLine("Pupille");
+else if (age <= 12)
+    Console.WriteLine("Minime");
+else if (age <= 17)
+    Console.WriteLine("Cadet");
+else
+    Console.WriteLine("Vous êtes trop vieux");
