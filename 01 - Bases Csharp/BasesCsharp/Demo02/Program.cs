37a3609f1523bde2becfaaeda7ce5e2c9933d19d
Guillaume MAIRESSE <guillaume@utopios.solutions>
2023-12-21 11:25:52 +0100

    Bases cs - sujets 17 18

--- 01 - Bases Csharp/BasesCsharp/Demo02/Program.cs
+++ 01 - Bases Csharp/BasesCsharp/Demo02/Program.cs
@@ -1,12 +1,50 @@
-﻿decimal compteEnBanque = 300;
-if (compteEnBanque >= 0)
+﻿string civilite = "Mme";
+switch (civilite)
 {
-    Console.WriteLine("Votre compte est créditeur");
+    case "M.":
+        Console.WriteLine("Bonjour monsieur");
+        break;
+    case "Mme":
+        Console.WriteLine("Bonjour madame");
+        break;
+    case "Mlle":
+        Console.WriteLine("Bonjour mademoiselle");
+        break;
+    default:
+        Console.WriteLine("Bonjour indéterminé");
+        break;
 }
+if (civilite == "M.")
+    Console.WriteLine("Bonjour monsieur");
+else if (civilite == "Mme")
+    Console.WriteLine("Bonjour madame");
+else if (civilite == "Mlle")
+    Console.WriteLine("Bonjour mademoiselle");
 else
-    Console.WriteLine("Votre compte est débiteur");
+    Console.WriteLine("Bonjour indéterminé");
 
-if (true)
+
+int age = 42;
+switch (age)
 {
-
+    case < 0 or > 125 :
+        Console.WriteLine("Age invalide");
+        break;
+    case < 18:
+        Console.WriteLine("Vous êtes mineur");
+        break;
+    case 40:
+        Console.WriteLine("Vous êtes quarantenaire");
+        break;
+    default:
+        Console.WriteLine("Vous êtes majeur mais pas quarantenaire");
+        break;
 }
+if (age < 0 || age > 125)
+    Console.WriteLine("Age invalide");
+else if (age < 18)
+    Console.WriteLine("Vous êtes mineur");
+else if (age == 40)
+    Console.WriteLine("Vous êtes quarantenaire");
+else
+    Console.WriteLine("Vous êtes majeur mais pas quarantenaire");
