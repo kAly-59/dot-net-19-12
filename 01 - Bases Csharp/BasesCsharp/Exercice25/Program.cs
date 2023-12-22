749ca39675a4e7a5d45b336584fb1cda72f431e3
Guillaume MAIRESSE <guillaume@utopios.solutions>
2023-12-22 14:28:04 +0100

    Bases cs - correction 24 25 + sujet triangle sapin

---
+++ 01 - Bases Csharp/BasesCsharp/Exercice25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("--- Gestion des notes ---\n\n");
+Console.WriteLine("Veuillez saisir 5 notes : \n\n");
+double max = 0, min = 20, somme = 0, moyenne;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("\t - Merci de saisir la note " + i + "(sur /20) :");
+    double note;
+    while (!double.TryParse(Console.ReadLine(), out note) || note < 0 || note > 20)
+        Console.WriteLine("Saisie invalide ! Réésayer :");
+    if (note > max)
+        max = note;
+    if (note < min)
+        min = note;
+    somme += note;
+}
+
+moyenne = somme / 5;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("\nla meilleure note est " + max + "/20");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("la moins bonne note est " + min + "/20");
+//Console.ForegroundColor = ConsoleColor.Gray;
+Console.ResetColor();
+Console.WriteLine("la moyenne des note est " + moyenne + "/20");
