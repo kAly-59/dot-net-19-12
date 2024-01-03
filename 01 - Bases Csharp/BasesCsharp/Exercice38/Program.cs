a3653a4ed4f1c1b8d663feb2575204b5c4abe8c8
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-03 12:25:48 +0100

    Bases cs - corrections 36-38

---
+++ 01 - Bases Csharp/BasesCsharp/Exercice38/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("--- Où est passé mon numéro ? ---");
+Console.WriteLine("Affectation des valeurs...");
+
+Random rnd = new Random();
+int[] valeurs = new int[10];
+
+for (int i = 0; i < valeurs.Length; i++)
+{
+    int num = rnd.Next(1, 51);
+    valeurs[i] = num;
+}
+
+Console.WriteLine("Affichage avant triage : ");
+
+// Affichage du tableau
+const string tabulation = "  ";
+string tabulations = tabulation;
+
+foreach (int val in valeurs)
+{
+    Console.WriteLine(tabulations + val);
+    tabulations += tabulation;
+}
+
+int premiereValeur = valeurs[0];
+
+Console.WriteLine("Après : ");
+
+// Triage du tableau et récupération de valeur
+
+Array.Sort(valeurs);
+
+tabulations = tabulation;
+foreach (int val in valeurs)
+{
+    Console.WriteLine(tabulations + val);
+    tabulations += tabulation;
+}
+
+int premiereValeurApresTriage = Array.IndexOf(valeurs, premiereValeur) + 1;
+
+Console.WriteLine($"Le nombre {premiereValeur} se trouvait en 1ere position");
+Console.WriteLine($"Il se retrouve à la position {premiereValeurApresTriage} après triage.");
+
