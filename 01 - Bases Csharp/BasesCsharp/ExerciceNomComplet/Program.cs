05b5b7676e20ffe50ae9220e359c16f55c7d8bac
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-03 15:17:04 +0100

    Bases cs - corrections nomComplet + soustraction

---
+++ 01 - Bases Csharp/BasesCsharp/ExerciceNomComplet/Program.cs
@@ -0,0 +1,9 @@
+﻿string NomComplet(string prenom, string nom)
+{
+    return $"{prenom} {nom}";
+}
+
+string nomPrenom = NomComplet("Benoit", "Lecoeuvre");
+Console.WriteLine(nomPrenom);
+
+Console.WriteLine(NomComplet("Guillaume", "Mairesse"));
