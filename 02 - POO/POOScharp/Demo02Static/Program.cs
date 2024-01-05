61e13dabd0c632c3bac15da2bcc5ec4f553e98b8
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-05 09:37:21 +0100

    POO - demo 2 static

--- 02 - POO/POOScharp/Demo02Static/Program.cs
+++ 02 - POO/POOScharp/Demo02Static/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Demo02Static.Classes;
+
+Console.WriteLine(Dinosaur.NombreDeDinos);
+
+Dinosaur[] dinoArray = {
+    new Dinosaur(),
+    new Dinosaur(),
+    new Dinosaur("Denver", 10, "Corythosaurus"),
+    new Dinosaur("Petit-Pied", 12, "Apatosaurus", true)
+};
+
+Console.WriteLine(Dinosaur.NombreDeDinos);
+
+
+Dinosaur.AfficherDinosaurVivants();
+
+Dinosaur.AfficherDinosaurCrees();
