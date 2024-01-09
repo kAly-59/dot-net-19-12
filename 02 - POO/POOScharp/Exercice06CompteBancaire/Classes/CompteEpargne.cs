bd57e4b4085d1c930ec1b595f121776ed8441fb2
Antoine D <antoine@utopios.net>
2024-01-09 16:01:25 +0100

    C# Avancé - Correction de l'exercice 06

---
+++ 02 - POO/POOScharp/Exercice06CompteBancaire/Classes/CompteEpargne.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice06CompteBancaire.Classes
+{
+    internal class CompteEpargne : CompteBancaire
+    {
+        // Un compte épargne se caractérise par la capacité à générer des revenus via un taux d'intérêt. On va donc devoir lier un Compte Epargne à un taux d'intérêts
+        private float _tauxInterets; 
+
+        public float TauxInterets { get => _tauxInterets; }
+
+        // Quand on créé un compte épargne, on va avoir un taux d'intérêt décidé dés création du compte
+        public CompteEpargne(Client client, float tauxInterets, decimal solde = 0m) : base(client, solde)
+        {
+            _tauxInterets = tauxInterets;
+        }
+
+        // Le code des dépots et des retrait est le même que pour un compte courant classique
+        public override bool Depot(decimal value)
+        {
+            _operations.Add(new Operation(value, TypeOperation.DEPOT));
+            _solde += value;
+            return true;
+        }
+
+        public override bool Retrait(decimal value)
+        {
+            if (_solde - value < 0) return false;
+
+            _operations.Add(new Operation(value, TypeOperation.RETRAIT));
+            _solde -= value;
+            return true;
+        }
+
+        // Pour un compte épargne, on doit pouvoir générer des revenus via une méthode prévue pour cela
+        public void Epargner()
+        {
+            _solde += decimal.Multiply(_solde, (decimal) _tauxInterets);
+        }
+    }
+}
---
+++ 02 - POO/POOScharp/Exercice06CompteBancaire/Classes/Operation.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercice06CompteBancaire.Classes
+{
+    internal class Operation
+    {
+        // Champ statique pour permettre un ID unique sur les opérations
+        private static int _count = 0;
+
+        // Champs d'instance pour chaque opération
+        private int _num;
+        private decimal _montant;
+        private TypeOperation _type;
+
+        // Propriétés pour permettre l'accès à l'extérieur des informations de chaque opération
+        public int Num { get => _num; }
+        public decimal Montant { get => _montant; }
+        public TypeOperation Type { get => _type; }
+
+        // Constructeur d'opération pour créer des opération en dehors de cette classe
+        public Operation(decimal montant, TypeOperation type)
+        {
+            _num = ++_count;
+            _montant = montant;
+            _type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"{_num}. [{Enum.GetName(_type)}] - {_montant}€";
+            // 1. [RETRAIT] 120.00€
+        }
+    }
+}
