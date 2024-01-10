2b81414623f9f864ca33b4d087352fff9484825f
kAly <kev.callet@gmail.com>
2024-01-10 16:31:27 +0100

    update

---
+++ 02 - POO/Reponses Exercices/ExoPoo_Figure/Classes/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ExoPoo_Figure.Classes
+{
+    public class Rectangle : Figure
+    {
+        private double _longueur;
+        private double _largeur;
+
+        public double Longueur { get => _longueur; set => _longueur = value; }
+        public double Largeur { get => _largeur; set => _largeur = value; }
+
+        public Rectangle(Point _origine, double Longueur, double Largeur) : base(_origine)
+        {
+            _longueur = Longueur;
+            _largeur = Largeur;
+        }
+
+        public override void Deplacement(double deplacementX, double deplacementY)
+        {
+            Origine.PosX += deplacementX;
+            Origine.PosY += deplacementY;
+            Console.WriteLine($"Le rectangle a été déplacé à {deplacementX},{deplacementY}");
+        }
+
+        public string CoordonneesDesCotes()
+        {
+            Point A = new Point(Origine.PosX, Origine.PosY);
+            Point B = new Point(Origine.PosX + _longueur, Origine.PosY);
+            Point C = new Point(Origine.PosX + _longueur, Origine.PosY + _largeur);
+            Point D = new Point(Origine.PosX, Origine.PosY + _largeur);
+
+            return
+                $"A = {A}\n" +
+                $"B = {B}\n" +
+                $"C = {C}\n" +
+                $"D = {D}\n";
+        }
+
+        public override string ToString()
+        {
+            return $"Coordonnées du rectangle ABCD (longueur = {_longueur}, largeur = {_largeur})";
+        }
+    }
+}
---
+++ 02 - POO/Reponses Exercices/ExoPoo_Figure/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExoPoo_Figure.Classes;
+
+public class Program
+{
+    public static void Main()
+    {
+        Point point = new Point(2, 2);
+
+        Carre carre = new Carre(point, 2);
+        Rectangle rectangle = new Rectangle(point, 3, 5);
+        Triangle triangle = new Triangle(point, 4, 5);
+
+        Console.WriteLine(carre.ToString());
+        Console.WriteLine(carre.CoordonneesDesCotes());
+        Console.WriteLine(rectangle.ToString());
+        Console.WriteLine(rectangle.CoordonneesDesCotes());
+        Console.WriteLine(triangle.ToString());
+        Console.WriteLine(triangle.CoordonneesDesCotes());
+
+        carre.Deplacement(3, 3);
+        Console.WriteLine(carre.ToString());
+        Console.WriteLine(carre.CoordonneesDesCotes());
+        rectangle.Deplacement(2, 2);
+        Console.WriteLine(rectangle.ToString());
+        Console.WriteLine(rectangle.CoordonneesDesCotes());
+        triangle.Deplacement(4, 4);
+        Console.WriteLine(triangle.ToString());
+        Console.WriteLine(triangle.CoordonneesDesCotes());
+
+    }
+}
