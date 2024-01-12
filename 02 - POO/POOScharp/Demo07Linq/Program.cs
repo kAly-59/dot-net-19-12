bc5b05de9894ed467b743059f964ca76c53bb9e1
Antoine D <antoine@utopios.net>
2024-01-12 10:28:42 +0100

    C# Avancé - Delegates et Linq

--- 02 - POO/POOScharp/Demo07Linq/Program.cs
+++ 02 - POO/POOScharp/Demo07Linq/Program.cs
@@ -46,10 +46,10 @@ var lesNombresPairs = mesNombres.Where(x => x % 2 == 0).ToDictionary(x => x.ToSt
 
 List<Personne> mesPersonnes = new()
 {
-    new() { Nom = "DUPONT", Prenom = "John"},
-    new() { Nom = "SCHMIT", Prenom = "Martha"},
-    new() { Nom = "DUPONT", Prenom = "Chloée"},
-    new() { Nom = "MARTEZ", Prenom = "Clark"}
+    new() { Nom = "DUPONT", Prenom = "John", Age = 47, Email = "j.dupont@gmail.com", Phone = "+33 147 741 256"},
+    new() { Nom = "SCHMIT", Prenom = "Martha", Age = 29, Email = "m.schmit@hotmail.com", Phone = "+33 159 236 478"},
+    new() { Nom = "DUPONT", Prenom = "Chloée", Age = 16, Email = "c.dupont@gmail.com", Phone = "+33 125 896 478"},
+    new() { Nom = "MARTEZ", Prenom = "Clark", Age = 47, Email = "c.martez@aol.com", Phone = "+32 147 852 369"}
 };
 
 // Pour trouver un élément dans une collection, on peut utiliser la fonction 
@@ -69,3 +69,81 @@ Personne chloeeAPartirDeLaFinSecurise = mesPersonnes.LastOrDefault(x => x.Prenom
 // A partir du début et s'assurer des non doublons de notre critère
 Personne laSeuleEtUniqueChloee = mesPersonnes.Single(x => x.Prenom == "Chloée");
 Personne? laSeuleEtUniqueChloeeSecure = mesPersonnes.SingleOrDefault(x => x.Prenom == "Chloée");
+
+// Via les méthodes .First() et .Last() ainsi que leurs versions sécurisées, on peut, sans critère de sélection, avoir la première et dernière valeur de l'énumerable
+var premierePersonneDeLaListe = mesPersonnes.FirstOrDefault();
+var dernierePersonneDeLaListe = mesPersonnes.LastOrDefault();
+
+// Si l'on veut désormais récupérer non pas une donnée mais plusieurs données, on peut utiliser .Where() suivi de critère de sélection également
+var personnesEntre18Et35Ans = mesPersonnes.Where(x => x.Age > 17 && x.Age < 36).ToHashSet();
+
+// Pour obtenir la série de données trié selon un critère, on peut utiliser .OrderBy() et .OrderByDescending()
+var personnesTriesParAgeCroissant = mesPersonnes.OrderBy(x => x.Age).ToHashSet();
+var personnesTriesParAgeDecroissant= mesPersonnes.OrderByDescending(x => x.Age).ToHashSet();
+var personnesTrieesParNomPuisPrenom = mesPersonnes.OrderBy(x => x.Nom).OrderBy(x => x.Prenom).ToHashSet();
+
+// Pour ne prendre qu'une seule valeur, on peut utiliser .Take()
+var personneLaPlusJeune = mesPersonnes.OrderBy(x => x.Age).Take(1);
+
+// Dans le cadre d'un paging, on a souvent recourt à l'utilisation de .Take() précédé de .Skip() en se basant sur une taille de page et un numéro de page. Ceci dans le but d'éviter d'envoyer à l'utilisateur toutes les valeurs d'un coup et de surcharger la base de données en cas de requêtage
+int nbPage = 0;
+var personnesALaPageX = mesPersonnes.Skip(50 * (nbPage - 1)).Take(50).ToHashSet();
+
+// Pour extraire une valeur d'une série, on peut utiliser plusieurs fonctions de base similaires à celles disponibles dans le SQL
+var ageTotalDesPersonnes = mesPersonnes.Sum(x => x.Age);
+var personneLaPlusVieille = mesPersonnes.Max(x => x.Age);
+var personneLaPlusJeuneBis = mesPersonnes.Min(x => x.Age);
+var moyenneDesAges = mesPersonnes.Average(x => x.Age);
+
+// Ou définir nous même notre code via la fonction .Aggregate()
+var chaineDesInitiales = mesPersonnes.Aggregate("Liste des initiales : ", (concat, element) =>
+{
+    if (mesPersonnes.Last() != element) return concat + element.Nom.Substring(0, 1).ToUpper() + ", ";
+    return concat + element.Nom.Substring(0, 1).ToUpper();
+});
+
+Console.WriteLine(chaineDesInitiales);
+
+
+var personnesAvecAgePair = mesPersonnes.Where(x => x.Age % 2 == 0);
+
+// Une autre syntaxe est disponible avec Linq, qui se rapproche du SQL. Celle-ci ne possède cependant pas toutes les features de la syntaxe avec les méthodes
+var personnesAvecAgePairBis = from p in mesPersonnes where p.Age % 2 == 0 select p;
+
+var personnesTrieesParNomDeFamille = from p in mesPersonnes orderby p.Nom select p;
+
+// On peut également réaliser des projections de données via la partie select / .Select();
+var nomsCompletsDesPersonnes = from p in mesPersonnes
+                               select new
+                               {
+                                   NomComplet = p.Prenom + " " + p.Nom
+                               };
+
+var nomsEtAgeAuCarreDesPersonnes = mesPersonnes.Select(x => new { Nom = x.Nom, AgeAuCarre = x.Age * x.Age });
+// var nomsEtAgeAuCarreDesPersonnes = mesPersonnes.Select(x => new { x.Nom, AgeAuCarre = x.Age * x.Age });
+
+foreach(var element in nomsEtAgeAuCarreDesPersonnes)
+{
+    Console.WriteLine($"{element.Nom} a comme âge au carre {element.AgeAuCarre}");
+}
+
+// var range = Enumerable.Range(0, 20);
+
+
+// Pour obtenir rapidement une série d'éléments aléatoires, on peut utiliser un énumérable temporaire non couteux et s'en servir pour brancher Linq dessus (Linq ne marche que sur les IEnumerable car c'est ce qu'il étend). Via la projection, on peut générer X valeurs et si l'on veut les récupérer par la suite dans un élément de type HashSet<T>
+var listePersonnesRandom = Enumerable.Range(1, 5).Select((x) =>
+{
+    return new Personne()
+    {
+        Nom = "Nom de la personne " + x,
+        Prenom = "Titi",
+        Age = x * 10,
+        Email = "temp.mail@email.com",
+        Phone = "+33 123 456 78" + x
+    };
+}).ToHashSet();
+
+foreach(Personne p in listePersonnesRandom)
+{
+    Console.WriteLine(p.Prenom + " " + p.Nom);
+}
