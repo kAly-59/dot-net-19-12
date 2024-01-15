db2626d30cf1490c3e9ff50c1d1a469d35ae01fd
Antoine D. <antoine@utopios.net>
2024-01-15 10:03:09 +0100

    demo exceptions

---
+++ 02 - POO/POOScharp/Demo09Exceptions/Personne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo09Exceptions
+{
+    internal class Personne
+    {
+
+        private int _age;
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value > 0 && value < 150)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new AgeException("L'âge doit être compris entre 1 et 149 ans");
+                }
+            }
+        }
+    }
+}
---
+++ 02 - POO/POOScharp/Demo09Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Demo09Exceptions;
+
+var p = new Personne();
+
+p.Prenom = "Arthur";
+p.Nom = "Dennetière";
+
+try
+{
+    p.Age = -3;
+} catch(Exception e)
+{
+    if(e is AgeException)
+    {
+        Console.WriteLine(e.Message);
+    } else
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
