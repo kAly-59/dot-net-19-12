64ee68460088d50204b4e3009cd61d4b8dcc5d60
Antoine D. <antoine@utopios.net>
2024-01-16 16:51:01 +0100

    demo pas fini dao

---
+++ 03 - ADO.NET/AdoCSharp/Demo02Dao/Classes/Maitre.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo02Dao.Classes
+{
+    internal class Maitre
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public List<Chien> Chiens { get; set; } = new();
+
+        public Maitre(int id, string nom, string prenom)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+        }
+
+        public Maitre(string nom, string prenom)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+    }
+}
---
+++ 03 - ADO.NET/AdoCSharp/Demo02Dao/Dao/ChienDao.cs
@@ -0,0 +1,28 @@
+﻿using Demo02Dao.Classes;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo02Dao.Dao
+{
+    internal class ChienDao : BaseDao<Chien>
+    {
+        public override List<Chien> GetAll()
+        {
+            using SqlConnection connection = DataConnection.GetConnection;
+        }
+
+        public override Chien Save(Chien element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Chien Update(Chien element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
