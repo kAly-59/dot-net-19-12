fdfe9f254aa03e4e8b05a7c9b8fa03cc7efc5daf
Arthur <arthur@utopios.solutions>
2024-01-17 16:49:39 +0100

    correction exercice02commande

---
+++ 03 - ADO.NET/AdoCSharp/Correction02Commande/Classes/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Correction02Commande.Classes
+{
+    internal class Client
+    {
+        public int Id { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Adresse { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string Telephone { get; set; }
+        public List<Commande> Commandes { get; set; } = new();
+
+        public override string ToString()
+        {
+            return @$"ID: {Id}
+Identité: {Prenom} {Nom}
+Telephone: {Telephone}
+Adresse: {Adresse} {CodePostal} {Ville}";
+        }
+
+        public Client(string prenom, string nom, string adresse, string codePostal, string ville, string telephone)
+        {
+            Prenom = prenom;
+            Nom = nom;
+            Adresse = adresse;
+            CodePostal = codePostal;
+            Ville = ville;
+            Telephone = telephone;
+        }
+
+        public Client(int id, string prenom, string nom, string adresse, string codePostal, string ville, string telephone): this(prenom, nom, adresse, codePostal, ville, telephone)
+        {
+            Id = id;
+        }
+    }
+}
