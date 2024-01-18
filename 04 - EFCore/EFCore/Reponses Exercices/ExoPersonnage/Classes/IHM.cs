462f40c14381d55ff4285aa8cd7caa3906bc8c75
kAly-59 <kev.callet@gmail.com>
2024-01-18 17:44:53 +0100

    update

---
+++ 04 - EFCore/EFCore/Reponses Exercices/ExoPersonnage/Classes/IHM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoPersonnage.Classes
+{
+    internal class IHM
+    {
+    }
+}
