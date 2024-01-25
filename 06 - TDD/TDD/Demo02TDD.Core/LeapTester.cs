32541e996feeae473c7015a21e3f531597dd9cdf
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-25 10:22:13 +0100

    TDD - demo 02 en tdd

---
+++ 06 - TDD/TDD/Demo02TDD.Core/LeapTester.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo02TDD.Core
+{
+    /*
+    On souhaite développer une application qui nous permet de connaître si une année est bissextile
+
+    Les critères pour une années sont :
+
+    - Une année divisible par 400.
+    - Une année divisible par 4 mais pas par 100.
+    - Une année n'est pas divisible par 4000.
+
+    Le développement doit se faire en TDD
+    */
+    public class LeapTester
+    {
+        public bool IsLeap(int year)
+        {
+            // INITIAL
+            //throw new NotImplementedException();
+
+            // GREEN 1
+            //if (year % 400 == 0)
+            //    return true;
+            //return false;
+
+            // GREEN 2
+            //if (year % 400 == 0)
+            //    return true;
+            //if (year % 4 == 0 && year % 100 != 0)
+            //    return true;
+            //return false;
+
+            // REFACTOR 2
+            //if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            //    return true;
+            //return false;
+
+            // GREEN 3
+            //if (year % 4000 == 0)
+            //    return false;
+            //if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            //    return true;
+            //return false;
+
+            // REFACTOR 3
+            return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) && year % 4000 != 0;
+        }
+    }
+}
---
+++ 06 - TDD/TDD/Demo02TDD.Tests/LeapTesterTest.cs
@@ -0,0 +1,54 @@
+﻿using Demo02TDD.Core;
+
+namespace Demo02TDD.Tests
+{
+    [TestClass]
+    public class LeapTesterTest
+    {
+        [DataTestMethod]
+        [DataRow(1200)]
+        [DataRow(400)]
+        public void TestLeap_400_ShouldBe_True(int year)
+        {
+            LeapTester lt = new();
+
+            bool res = lt.IsLeap(year);
+
+            Assert.IsTrue(res);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(12)]
+        [DataRow(404)]
+        public void TestLeap_4_Not_100_ShouldBe_True(int year)
+        {
+            LeapTester lt = new();
+
+            bool res = lt.IsLeap(year);
+
+            Assert.IsTrue(res);
+        }
+
+        [DataTestMethod]
+        [DataRow(8000)]
+        [DataRow(12000)]
+        public void TestLeap_4000_ShouldBe_False(int year)
+        {
+            LeapTester lt = new();
+
+            bool res = lt.IsLeap(year);
+
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void TestLeap_Others_ShouldBe_False()
+        {
+            LeapTester lt = new LeapTester();
+
+            bool res = lt.IsLeap(1999);
+            Assert.IsFalse(res);
+        }
+    }
+}
