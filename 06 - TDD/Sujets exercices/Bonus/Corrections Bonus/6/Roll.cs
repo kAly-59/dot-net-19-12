985dd279270b0cafeb39ea8835a64be91e32edfd
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-26 16:41:40 +0100

    TDD - corrections bonus

---
+++ 06 - TDD/Sujets exercices/Bonus/Corrections Bonus/6/Roll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTDD
+{
+    public class Roll
+    {
+        private int pins;
+        
+        public Roll(int p) { 
+            Pins= p;
+        }
+
+        public int Pins { get => pins; set => pins = value; }
+    }
+}
