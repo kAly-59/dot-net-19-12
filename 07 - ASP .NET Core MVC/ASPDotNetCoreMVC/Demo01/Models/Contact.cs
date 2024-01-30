92dc1cbba31d87de0ce5eeefb0621785d7c838a6
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-30 16:24:34 +0100

    MVC - DisplayNameFor

--- 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Demo01/Models/Contact.cs
+++ 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Demo01/Models/Contact.cs
@@ -1,10 +1,19 @@
-﻿namespace Demo01.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo01.Models;
 
 public class Contact
 {
+    // L'annotation Display précise comment on devrait afficher une certaine propriété dans la partie présentation
+    // Elle sera utile pour Html.DisplayNameFor(...)
+
     public int Id { get; set; }
+    [Display(Name = "Prénom")] // par défaut le display correspond au nom de la propriété => "FirstName"
     public string? FirstName { get; set; }
+    [Display(Name = "Nom")]
     public string? LastName { get; set; }
+    [Display(Name = "Adresse Mail")]
     public string? Email { get; set; }
+    [Display(Name = "Numéro de téléphone")]
     public string? Phone { get; set; }
 }
