f15300619bbd7c87c2a728a17caee13d0e9bdc7d
Guillaume Mairesse <guillaume@utopios.solutions>
2024-01-31 09:40:53 +0100

    MVC - FakeContactDb sans Injection de dépendances

--- 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Demo01/Controllers/ContactsController.cs
+++ 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Demo01/Controllers/ContactsController.cs
@@ -1,17 +1,19 @@
-﻿using Demo01.Models;
+﻿using Demo01.Data;
+using Demo01.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo01.Controllers
 {
     public class ContactsController : Controller
     {
-        private List<Contact> _contactList = new List<Contact>()
-        {
-            new Contact { Id = 1, FirstName = "Bob", LastName="Marley", Email="bobo@ganjamail.com", Phone="0607080910"},
-            new Contact { Id = 2, FirstName = "Elvis", LastName="Presley", Email="elvis@rock.com", Phone="0607080911"},
-            new Contact { Id = 3, FirstName = "Michael", LastName="Jackson", Email="mj@popking.com", Phone="0607080912"},
-        };
+        //private List<Contact> _contactList = new List<Contact>()
+        //{
+        //    new Contact { Id = 1, FirstName = "Bob", LastName="Marley", Email="bobo@ganjamail.com", Phone="0607080910"},
+        //    new Contact { Id = 2, FirstName = "Elvis", LastName="Presley", Email="elvis@rock.com", Phone="0607080911"},
+        //    new Contact { Id = 3, FirstName = "Michael", LastName="Jackson", Email="mj@popking.com", Phone="0607080912"},
+        //};
 
+        private FakeContactDb _fakeContactDb = new FakeContactDb();
 
         // /Contacts/       => possible grace au app.MapControllerRoute("default", ...) de program.cs
         // /Contacts/Index
@@ -21,11 +23,23 @@ namespace Demo01.Controllers
             //return "Je suis la page pour afficher les contacts.";
             //return View();
 
-           
+
             //ViewData["contactList"] = contactList;
             //ViewBag.ContactList = contactList;
 
-            return View(_contactList); // View(model)
+            //return View(_contactList); // View(model)
+
+            Contact? mich = new Contact()
+            {
+                FirstName = "Michel",
+                LastName = "Sardou",
+                Email = "michmich@sardonux.fr",
+                Phone = "0607080945"
+            };
+
+            _fakeContactDb.Add(mich);
+
+            return View(_fakeContactDb.GetAll());
         }
         // /Contacts/Details/5
         public IActionResult Details(int id)
@@ -39,10 +53,28 @@ namespace Demo01.Controllers
             //ViewData["contact"] = contact;
             //ViewBag.Contact = contact;
 
-            Contact? contact = _contactList.FirstOrDefault(c => c.Id == id);
+            //Contact? contact = _contactList.FirstOrDefault(c => c.Id == id);
+
+            Contact? contact = _fakeContactDb.GetById(id);
 
             return View(contact);
         }
+
+        public IActionResult AddMichel()
+        {
+            Contact? mich = new Contact()
+            {
+                FirstName = "Michel",
+                LastName = "Sardou",
+                Email = "michmich@sardonux.fr",
+                Phone = "0607080945"
+            };
+
+            _fakeContactDb.Add(mich);
+
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Add()
         {
             //return "Je suis la page pour ajouter un contact...";
---
+++ 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Demo01/Data/FakeContactDb.cs
@@ -0,0 +1,64 @@
+﻿using Demo01.Models;
+
+namespace Demo01.Data
+{
+    public class FakeContactDb // représente une fausse base de données(équivalent à un DbContext ou un Repository)
+    {
+        private List<Contact> _contacts; // équivalent de la base de données
+        private int _lastId = 0; // pour faire un équivalent d'IDENTITY ou AUTO INCREMENT
+
+        public FakeContactDb()
+        {
+            _contacts = new List<Contact>() // équivalent du data seed (données par défaut)
+            {
+                new Contact { Id = ++_lastId, FirstName = "Bob", LastName="Marley", Email="bobo@ganjamail.com", Phone="0607080910"},
+                new Contact { Id = ++_lastId, FirstName = "Elvis", LastName="Presley", Email="elvis@rock.com", Phone="0607080911"},
+                new Contact { Id = ++_lastId, FirstName = "Michael", LastName="Jackson", Email="mj@popking.com", Phone="0607080912"},
+            };
+        }
+
+        public List<Contact> GetAll()
+        {
+            return _contacts;
+        }
+
+        public Contact? GetById(int id)
+        {
+            return _contacts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public bool Add(Contact contact)
+        {
+            contact.Id = ++_lastId;
+            _contacts.Add(contact);
+            return true; // l'ajout s'est bien passé
+        }
+
+        public bool Edit(Contact contact)
+        {
+            var contactFromDb = GetById(contact.Id);
+
+            if (contactFromDb == null)
+                return false;
+
+            contactFromDb.FirstName = contact.FirstName;
+            contactFromDb.LastName = contact.LastName;
+            contactFromDb.Email = contact.Email;
+            contactFromDb.Phone = contact.Phone;
+
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var contact = GetById(id);
+
+            if (contact == null)
+                return false;
+
+            _contacts.Remove(contact);
+
+            return true;
+        }
+    }
+}
