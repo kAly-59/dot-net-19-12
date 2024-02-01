297add37a287747c4d416c05ab0a7197246d619b
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-01 12:00:14 +0100

    MVC - avec DataSeed

--- 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Exercice04/Data/ApplicationDbContext.cs
+++ 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Exercice04/Data/ApplicationDbContext.cs
@@ -19,5 +19,22 @@ namespace Exercice04.Data
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\AspMvc;Integrated Security=True");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // changements contenus dans une migration
+        {
+            // Data seed
+            //modelBuilder.Entity<Animal>().HasData(new Animal() { Id=1, FirstName = "Coco", Age = 3, Species = "Perroquet" }); // ne pas oublier l'id
+
+            int lastIndex = 0;
+            var animals = new List<Animal>()
+            {
+                new Animal() { Id=++lastIndex, FirstName = "Coco", Age = 3, Species = "Perroquet" },
+                new Animal() { Id=++lastIndex, FirstName = "Pepette", Age = 5, Species = "Chien, Bouledogue" },
+                new Animal() { Id=++lastIndex, FirstName = "Perry", Age = 15, Species = "Platypus" },
+            };
+
+            modelBuilder.Entity<Animal>().HasData(animals);
+
+        }
     }
 }
---
+++ 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Exercice04/Migrations/20240201105636_DataSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exercice04.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "Age", "FirstName", "Species" },
+                values: new object[] { 1, 3, "Coco", "Perroquet" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "Age", "FirstName", "Species" },
+                values: new object[] { 2, 5, "Pepette", "Chien, Bouledogue" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "Age", "FirstName", "Species" },
+                values: new object[] { 3, 15, "Perry", "Platypus" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
