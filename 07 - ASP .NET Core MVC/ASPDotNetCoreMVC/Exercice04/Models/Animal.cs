7640de82c87562172cfa438e8fa78450c30ae70d
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-02 12:27:13 +0100

    MVC - model validation

--- 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Exercice04/Models/Animal.cs
+++ 07 - ASP .NET Core MVC/ASPDotNetCoreMVC/Exercice04/Models/Animal.cs
@@ -6,8 +6,12 @@ public class Animal
 {
     public int Id { get; set; }
     [Display(Name = "Prénom de l'animal")]
+    [Required(ErrorMessage = "Le nom est requis.")]
+    [StringLength(30, MinimumLength = 3, ErrorMessage = "Le nom doit être compris entre 3 et 30 caractères.")]
     public string? FirstName { get; set; }
     [Display(Name = "Age de l'animal")]
+    [Required(ErrorMessage = "L'age est requis.")]
+    [Range(0, 120, ErrorMessage = "L'age doit être compris entre 0 et 120")]
     public int? Age { get; set; }
     [Display(Name = "Espèce de l'animal")]
     public string? Species { get; set; }
