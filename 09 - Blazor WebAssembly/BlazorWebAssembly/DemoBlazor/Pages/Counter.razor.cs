bbf61ab592d5967b1b0d16eb42375614e143511d
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-12 12:02:11 +0100

    Blazor - code-behind partial ou héritage

---
+++ 09 - Blazor WebAssembly/BlazorWebAssembly/DemoBlazor/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Pages
+{
+    public partial class Counter
+    //public class CounterBase : ComponentBase
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount += 10;
+        }
+
+        private void DecrementCount()
+        {
+            currentCount -= 10;
+        }
+    }
+}
