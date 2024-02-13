8dc4dd661e6c78d87f9141b9a456baa810e6ff55
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-13 14:54:27 +0100

    Blazor - Model validation + exercice formulaire

--- 09 - Blazor WebAssembly/BlazorWebAssembly/DemoBlazor/Models/Gorilla.cs
+++ 09 - Blazor WebAssembly/BlazorWebAssembly/DemoBlazor/Models/Gorilla.cs
@@ -1,11 +1,17 @@
-﻿namespace DemoBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoBlazor.Models
 {
     public class Gorilla
     {
         public int Id { get; set; }
+        [Required, StringLength(12, MinimumLength = 3, ErrorMessage = "Le nom doit faire entre 3 et 12 caractères")]
         public string? Name { get; set; }
         public string? Description { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Le gorille doit avoir une banane")]
         public bool OwnsBanana { get; set; }
+        [Required]
+        [Range(1, 5, ErrorMessage = "La couleur est requise.")]
         public FurColor FurColor { get; set; }
 
         public override string ToString()
@@ -15,6 +21,7 @@
     }
     public enum FurColor
     {
+        Nothing,
         Red,
         Brown,
         Pink,
