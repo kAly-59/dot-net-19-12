b53b4e0ebb64788696b91c7b36cb1f41ab0e0912
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-13 14:34:13 +0100

    Blazor - EditForm

---
+++ 09 - Blazor WebAssembly/BlazorWebAssembly/DemoBlazor/Models/Gorilla.cs
@@ -0,0 +1,24 @@
+﻿namespace DemoBlazor.Models
+{
+    public class Gorilla
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool OwnsBanana { get; set; }
+        public FurColor FurColor { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Description} {OwnsBanana} {FurColor}";
+        }
+    }
+    public enum FurColor
+    {
+        Red,
+        Brown,
+        Pink,
+        Orange,
+        White
+    }
+}
