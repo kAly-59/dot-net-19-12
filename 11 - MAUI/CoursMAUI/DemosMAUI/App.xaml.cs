67e9b281b999e16c50e7e0175b4d03690544f84c
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-19 11:34:55 +0100

    MAUI - creation du projet

---
+++ 11 - MAUI/CoursMAUI/DemosMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DemosMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
