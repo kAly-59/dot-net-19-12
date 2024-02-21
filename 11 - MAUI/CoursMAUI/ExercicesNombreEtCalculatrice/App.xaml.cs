81d25a04fe97a55103ed422ab5f97465f7c39eab
Guillaume Mairesse <guillaume@utopios.solutions>
2024-02-21 09:50:15 +0100

    MAUI - correction Nb mystere et Calculatrice

---
+++ 11 - MAUI/CoursMAUI/ExercicesNombreEtCalculatrice/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercicesNombreEtCalculatrice
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
